281c17be0ff7fa06c2eaf2afad7a60aeb6824fc0
25LP1579 25LP1579 <25LP1579@G304-CC0223-2.local>
2025-12-26 12:12:18 +0800

    testing

--- src/app/about/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from "react";
-
-const AboutLayout = ({ children }: Readonly<PropsWithChildren>) => {
-  return <div>{children}</div>;
-};
-
-export default AboutLayout;
